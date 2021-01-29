c58a282347c4d887eec1bb7940b6da0b6fa682d3
ayman <aymanalbaz98@gmail.com>
2021-01-29 18:11:49 -0500

    Slight performance tweak

--- src/main/julia/measure.sh
+++ src/main/julia/measure.sh
@@ -1,6 +1,24 @@
+#!/usr/bin/env bash
 # To download Julia visit https://julialang.org/downloads/
-echo "<Performance for $(basename "$PWD")>"
+# Julia will use its own timer because the first time you run it, it will compile.
+# Internally it runs once to compile, and then measures the time of the second run
+dir=$(cd $(dirname $0); pwd -P)
+cd $dir
+
+tries=5
+
+if [ ! -z "$1" ]; then
+  tries=$1
+fi
+
+echo "Performance for $(basename $dir)"
 julia --version
 
-julia -O3 performance.jl
-echo "</Performance for $(basename "$PWD")>"
+echo "Building..."
+
+echo "Running..."
+for i in $(jot - 1 $tries); do
+  julia -O3 performance.jl
+done
+
+echo "Done!"
