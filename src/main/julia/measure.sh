451e222d52a89bc339731ecdc79a46e2b9b0245d
ayman <aymanalbaz98@gmail.com>
2021-01-28 22:01:45 -0500

    echo fix

--- src/main/julia/measure.sh
+++ src/main/julia/measure.sh
@@ -1,8 +1,6 @@
 # To download Julia visit https://julialang.org/downloads/
-echo
 echo "<Performance for $(basename "$PWD")>"
 julia --version
 
 julia -O3 performance.jl
 echo "</Performance for $(basename "$PWD")>"
-echo
