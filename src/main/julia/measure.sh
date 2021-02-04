be1d75baca10f9937f23afb99cb155d2eadc169a
ayman <aymanalbaz98@gmail.com>
2021-02-03 19:51:50 -0500

    Made structs immutable

--- src/main/julia/measure.sh
+++ src/main/julia/measure.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 # To download Julia visit https://julialang.org/downloads/
-# Julia will use its own timer because the first time you run it, it will compile.
-# Internally it runs once to compile, and then measures the time of the second run
 dir=$(cd $(dirname $0); pwd -P)
 cd $dir
 
 tries=5
-
 if [ ! -z "$1" ]; then
   tries=$1
 fi
@@ -15,10 +12,11 @@ echo "Performance for $(basename $dir)"
 julia --version
 
 echo "Building..."
+time echo "Nothing to do"
 
 echo "Running..."
 for i in $(jot - 1 $tries); do
-  julia -O3 performance.jl
+  time julia -O2 performance.jl
 done
 
 echo "Done!"
